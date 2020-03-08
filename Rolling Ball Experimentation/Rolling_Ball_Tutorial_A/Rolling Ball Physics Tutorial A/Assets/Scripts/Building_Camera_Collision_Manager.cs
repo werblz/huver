7594e6f5f98eb529c151cf6d4acdca41d8838459
werblz <sean@huxter.org>
2020-03-08 13:43:20 -0400

    First working version of camera/buildling collision. Camera has a script on it that finds the Building_Camera_Collision_Manager, and uses it to call a method on the colliding buidling to swap out the opaque rendermesh with translucent, and vice versa when not colliding.
    Building Manager has the methods to do just that, and holds references to the opaque Rednermesh and translucent rendermesh.
    
    Opaque buildings have the normal materials. Translucent ones have the materials change to be translucent.

---
+++ Rolling Ball Experimentation/Rolling_Ball_Tutorial_A/Rolling Ball Physics Tutorial A/Assets/Scripts/Building_Camera_Collision_Manager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building_Camera_Collision_Manager : MonoBehaviour {
+
+    // New plan. Each building will have another mesh with the proper materials already on it.
+
+    [SerializeField]
+    private MeshRenderer opaqueBuilding = null;
+
+    [SerializeField]
+    private MeshRenderer transBuilding = null;
+
+    
+
+
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetOpaque();
+	}
+
+    public void SetTransparent()
+    {
+        transBuilding.enabled = true;
+        opaqueBuilding.enabled = false;
+    }
+
+    public void SetOpaque()
+    {
+        transBuilding.enabled = false;
+        opaqueBuilding.enabled = true;
+    }
+
+
+    /* 
+     * 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "CameraCollider")
+        {
+            SetTransparent();
+            Debug.Log("YIKES! YOU HIT A BUILDING BACK THERE!");
+
+            // Test code: Make building tiny on collision, normalsize on not.
+            // originalBuildingScale = other.gameObject.transform.localScale;
+            // other.gameObject.transform.localScale = new Vector3 ( 1.0f, 1.0f, 1.0f);
+            
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "CameraCollider")
+        {
+
+            SetOpaque();
+
+            Debug.Log("You left that building!");
+            //other.gameObject.SetActive(true);
+            //other.gameObject.transform.localScale = originalBuildingScale;
+        }
+
+    }
+
+    */
+}
